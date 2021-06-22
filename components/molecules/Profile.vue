1aa1966299b77a674974bbd1899d33ca52411892
HerbertSousa <herberti1405@outlook.com>
2021-06-22 18:39:40 -0300

    profile and completed challenges

---
+++ components/molecules/Profile.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="flex items-center">
+		<img src="https://github.com/HerbertSousa.png" alt="Herbert" class="mr-6 w-20 h-20 rounded-full">
+		<div class="flex flex-col w-full">
+			<strong class="text-title text-2xl">Herbert</strong>
+			<p class="flex items-center">
+				<img src="icons/level.svg" alt="levelup" class="mr-2">
+				<span class="text-base">Level {{ level }}</span>
+			</p>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { mapState } from 'vuex';
+
+export default Vue.extend({
+	computed: mapState('Challenges', ['level']),
+});
+</script>
